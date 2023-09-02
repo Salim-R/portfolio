2e773539cd739e20b03c4a5748f37acabd3a5d70
Salim-R <96195346+Salim-R@users.noreply.github.com>
2023-09-02 20:15:15 +0200

    5

--- pages/index.js
+++ pages/index.js
@@ -138,7 +138,7 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Depuis le début de mon parcours, j'ai travaillé sur divers projets effectués pendant ma
+              Depuis le début de mon parcours, j'ai travaillé sur divers projets
               avec des outils à jour.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
